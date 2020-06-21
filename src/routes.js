90db8ddb0cbe75e7b9cf9b1e1281f4d2e3fb719b
Henrique Barbosa de Souza Junior <henrique@hbveterinaria.com.br>
2020-06-21 18:06:51 -0300

    Tratamento de error / prevent GET /favicon.ico

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,6 @@ routes.get('/produto/last', ProdController.listLast);
 routes.post('/produtos', ProdController.create);
 routes.put('/produtos/:cod', ProdController.update);
 routes.delete('/produtos/:cod',ProdController.delete);
+routes.get('/favicon.ico', (req, res) => res.status(204));
 
 module.exports = routes; 
